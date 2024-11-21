60dc097c2300fb2b22ae9f17797887f019b1fd22
Huyen Vu <huyenvtthe172681@fpt.edu.vn>
2024-11-22 03:17:01 +0700

    routing admin page

--- src/app/pages/admin-page/admin-page-routing.module.ts
+++ src/app/pages/admin-page/admin-page-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { KhoaHocComponent } from './khoa-hoc/khoa-hoc.component';
 import { ChiTietKhoaHocComponent } from './chi-tiet-khoa-hoc/chi-tiet-khoa-hoc.component';
 import { TongQuanComponent } from './tong-quan/tong-quan.component';
+import { InformationComponent } from './chi-tiet-khoa-hoc/information/information.component';
+import { ChuongTrinhHocComponent } from './chi-tiet-khoa-hoc/chuong-trinh-hoc/chuong-trinh-hoc.component';
+import { ScoresComponent } from './chi-tiet-khoa-hoc/scores/scores.component';
+import { FeedbackComponent } from './chi-tiet-khoa-hoc/feedback/feedback.component';
 
 const routes: Routes = [
   {
@@ -21,26 +25,17 @@ const routes: Routes = [
       {
         path: 'chi-tiet-khoa-hoc/:id',
         component: ChiTietKhoaHocComponent,
+        children: [
+          { path: '0', component: InformationComponent },
+          { path: '1', component: ChuongTrinhHocComponent },
+          { path: '2', component: ScoresComponent },
+          { path: '3', component: FeedbackComponent },
+          { path: '', redirectTo: '0', pathMatch: 'full' },
+        ],
       },
     ],
   },
 ];
-
-// const routes: Routes = [
-//   {
-//     path: '',
-//     component: TongQuanComponent,
-//   },
-//   {
-//     path: 'quan-tri/khoa-hoc',
-//     component: KhoaHocComponent,
-//   },
-//   {
-//     path: 'quan-tri/chi-tiet-khoa-hoc/:id',
-//     component: ChiTietKhoaHocComponent,
-//   },
-// ];
-
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
