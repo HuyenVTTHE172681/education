ebe8b664bab8a680e951f36d92c440f8988e60f9
Huyen Vu <huyenvtthe172681@fpt.edu.vn>
2024-12-10 16:27:52 +0700

    dashboard course

--- src/app/pages/admin-page/tong-quan/dashboard-course/dashboard-course.component.ts
+++ src/app/pages/admin-page/tong-quan/dashboard-course/dashboard-course.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClassRoomService } from '../../../../services/classRoom.service';
+import { CourseService } from '../../../../services/course.service';
+import { DashboardService } from '../../../../services/dashboard.service';
+import { TeacherService } from '../../../../services/teacher.service';
+import { IResponeList } from '../../../../models/common.model';
+import { ClassRoom } from '../../../../models/classRoom.model';
+import { Subject as SubjectModel } from '../../../../models/subject.model';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-course',
   templateUrl: './dashboard-course.component.html',
   styleUrl: './dashboard-course.component.css'
 })
-export class DashboardCourseComponent {
+export class DashboardCourseComponent implements OnInit {
+  adminCourse: any[] = [];
+  page: number = 1;
+  size: number = 1000;
+  filter: string = '';
+  selectedClassroom: string | undefined;
+  selectedSubject: string | undefined;
+  accountId: string = '';
+  classRoom: ClassRoom[] = [];
+  subject: SubjectModel[] = [];
+  searchText: string = '';
 
+  private searchSubject: Subject<string> = new Subject(); // Subject for search
+  constructor(private classRoomSrv: ClassRoomService, private courseSrv: CourseService, private dashboardSrv: DashboardService, private teacherSrv: TeacherService) {
+  }
+
+  ngOnInit(): void {
+    this.getDashboardAdminCourse();
+
+    this.getClassRoom();
+  }
+
+  getDashboardAdminCourse() {
+    this.dashboardSrv.getDashboardAdminCourse(this.page, this.size, this.filter, this.selectedClassroom || '', this.selectedSubject || '', this.accountId).subscribe({
+      next: (data: IResponeList<any>) => {
+        this.adminCourse = data.data.data;
+        console.log("Admin course: ", this.adminCourse)
+      }
+    })
+  }
+
+  // Get classrom
+  getClassRoom() {
+    this.classRoomSrv
+      .getClassRooms(this.page, this.size, this.searchText)
+      .subscribe({
+        next: (data: IResponeList<ClassRoom>) => {
+          this.classRoom = data.data.data;
+        },
+      });
+  }
+
+  // Get subject
+  getSubjectsByClassRoomId(classRoomId: string): void {
+    this.courseSrv
+      .getSubject(classRoomId, this.searchText, this.page, this.size)
+      .subscribe({
+
+        next: (response) => {
+          this.subject = response;
+        },
+        error: () => {
+          console.error('Error fetching subjects.');
+        },
+      });
+  }
+
+  onSearchChange(searchValue: string): void {
+    this.filter = searchValue.trim();
+    this.searchSubject.next(this.filter);
+  }
+  searchCourse(): void {
+    console.log('Searching with filter:', this.filter);
+    this.page = 1;
+    this.getDashboardAdminCourse();
+  }
+
+  resetFilters(): void {
+    this.selectedClassroom = undefined;
+    this.selectedSubject = undefined;
+
+    this.filter = '';
+    this.page = 1;
+
+
+    this.getDashboardAdminCourse();
+  }
 }
