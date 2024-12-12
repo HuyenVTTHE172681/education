c2f50710d1776be0ebac4b610c501ebc5f44be09
Huyen Vu <huyenvtthe172681@fpt.edu.vn>
2024-12-12 10:30:19 +0700

    filter date with day, week, month, year

--- src/app/pages/admin-page/tong-quan/tong-quan.component.ts
+++ src/app/pages/admin-page/tong-quan/tong-quan.component.ts
@@ -21,7 +21,8 @@ export class TongQuanComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getDashboardFilterByDate();
+    this.updateDateRange('day');
+    this.getDashboardFilterByDate(this.fromDate, this.toDate);
 
     this.dateFilter = [
       { name: 'Hôm nay', code: 'day' },
@@ -60,7 +61,7 @@ export class TongQuanComponent implements OnInit {
 
   }
 
-  getDashboardFilterByDate() {
+  getDashboardFilterByDate(fromDate: string, toDate: string) {
     this.dashboardSrv.getDashboardFilterByDate(this.fromDate, this.toDate).subscribe({
       next: (data: any) => {
         this.dashboardFilterByDate = data.data.dashboardOverview;
@@ -69,6 +70,64 @@ export class TongQuanComponent implements OnInit {
         console.log("Dashboad: ", this.adviceRequest);
       },
     })
+  }
+
+  // updateDateRange(filter: string) {
+  //   const now = new Date();
+  //   switch (filter) {
+  //     case 'day':
+  //       this.fromDate = now.toISOString().split('T')[0] + '%2000:00:00';
+  //       this.toDate = now.toISOString().split('T')[0] + '%2023:59:59';
+  //       break;
+  //     case 'week':
+  //       const firstDayOfWeek = new Date(now.getFullYear(), now.getMonth(), now.getDate() - now.getDay());
+  //       this.fromDate = firstDayOfWeek.toISOString().split('T')[0] + '%2000:00:00';
+  //       this.toDate = now.toISOString().split('T')[0] + '%2023:59:59';
+  //   }
+
+  // }
+
+  onDateFilterChange() {
+    if (this.selectedDateFilter) {
+      this.updateDateRange(this.selectedDateFilter);
+      this.getDashboardFilterByDate(this.fromDate, this.toDate);
+    }
+  }
+  updateDateRange(filter: string) {
+    const now = new Date();
+    switch (filter) {
+      case 'day':
+        this.fromDate = this.formatDate(now, '00:00:00');
+        this.toDate = this.formatDate(now, '23:59:59');
+        break;
+      case 'week':
+        const firstDayOfWeek = this.startOfWeek(now);
+        this.fromDate = this.formatDate(firstDayOfWeek, '00:00:00');
+        this.toDate = this.formatDate(new Date(now.setDate(firstDayOfWeek.getDate() + 6)), '23:59:59');
+        break;
+      case 'month':
+        const firstDayOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
+        const lastDayOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+        this.fromDate = this.formatDate(firstDayOfMonth, '00:00:00');
+        this.toDate = this.formatDate(lastDayOfMonth, '23:59:59');
+        break;
+      case 'year':
+        const firstDayOfYear = new Date(now.getFullYear(), 0, 1);
+        const lastDayOfYear = new Date(now.getFullYear(), 11, 31);
+        this.fromDate = this.formatDate(firstDayOfYear, '00:00:00');
+        this.toDate = this.formatDate(lastDayOfYear, '23:59:59');
+        break;
+    }
+  }
+  startOfWeek(date: Date): Date {
+    const firstDay = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
+    return new Date(date.setDate(firstDay));
+  }
+
+  formatDate(date: Date, time: string): string {
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2); return `${year}-${month}-${day} ${time}`;
   }
 
 
