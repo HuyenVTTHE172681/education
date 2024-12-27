244e742d08a867347f0ce5fd2065bb9ad26ea549
Huyen Vu <huyenvtthe172681@fpt.edu.vn>
2024-12-27 17:06:41 +0700

    patch form

--- src/app/pages/admin-page/chi-tiet-cau-hoi/chi-tiet-cau-hoi.component.ts
+++ src/app/pages/admin-page/chi-tiet-cau-hoi/chi-tiet-cau-hoi.component.ts
@@ -5,6 +5,7 @@ import { TestQuestionGroup, TestQuestionNewById, TestQuestionType } from '../../
 import { ActivatedRoute } from '@angular/router';
 import { Teacher } from '../../../core/models/teacher.model';
 import { TeacherService } from '../../../core/services/teacher.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-chi-tiet-cau-hoi',
@@ -22,23 +23,55 @@ export class ChiTietCauHoiComponent implements OnInit {
     status: 1
   }
   levelQuestion = [
-    { name: 'Tất cả', value: -1 },
-    { name: 'Dễ', value: 1 },
-    { name: 'Trung bình', value: 2 },
-    { name: 'Khó', value: 3 },
-    { name: 'Rất khó', value: 4 },
+    { name: 'Tất cả', code: -1 },
+    { name: 'Dễ', code: 1 },
+    { name: 'Trung bình', code: 2 },
+    { name: 'Khó', code: 3 },
+    { name: 'Rất khó', code: 4 },
   ]
   teacher: Teacher[] = [];
   testQuestionType: TestQuestionType[] = [];
   testQuestionGroup: TestQuestionGroup[] = [];
   questionNew: TestQuestionNewById = new TestQuestionNewById();
+  questionForm: FormGroup;
 
   constructor(
     private questionsSrv: QuestionsService,
     private route: ActivatedRoute,
     private teacherSrv: TeacherService,
-    private questionSrv: QuestionsService
-  ) { }
+    private questionSrv: QuestionsService,
+    private formBuilder: FormBuilder
+  ) {
+    this.questionForm = this.formBuilder.group({
+      answer: [''],
+      attackFiles: [''],
+      content: [''],
+      createdBy: [''],
+      createdDate: [''],
+      description: [''],
+      id: [''],
+      isAdd: [0],
+      isAutoSort: [0],
+      isLayoutSplitVertical: [1],
+      lessonLink: [''],
+      level: [1],
+      modifiedBy: [''],
+      modifiedDate: [''],
+      name: [''],
+      order: [0],
+      point: [0],
+      publicStatus: [0],
+      quizzConfigSets: [''],
+      teacherIds: [''],
+      testId: [''],
+      testQuestionAnswers: [[]],
+      testQuestionGroupId: [0],
+      testQuestionGroupName: [''],
+      testQuestionTypeCode: [''],
+      testQuestionTypeName: [''],
+      totalFiltered: [''],
+    })
+  }
 
   ngOnInit(): void {
     this.initParams();
@@ -82,7 +115,41 @@ export class ChiTietCauHoiComponent implements OnInit {
     this.questionSrv.getTestQuestionType(this.query.filter, this.query.page, this.query.size).subscribe(res => {
       if (res.statusCode === 200) {
         this.testQuestionType = res.data.data;
+        
       }
+    })
+  }
+
+  patchQuestionForm(question: any) {
+    this.questionForm.patchValue({
+      answer: question.answer,
+      attackFiles: question.attackFiles,
+      content: question.content,
+      createdBy: question.createdBy,
+      createdDate: question.createdDate,
+      description: question.description,
+      id: question.id,
+      isAdd: question.isAdd,
+      isAutoSort: question.isAutoSort,
+      isLayoutSplitVertical: question.isLayoutSplitVertical,
+      lessonLink: question.lessonLink,
+      level: question.level,
+      modifiedBy: question.modifiedBy,
+      modifiedDate: question.modifiedDate,
+      name: question.name,
+      order: question.order,
+      point: question.point,
+      publicStatus: question.publicStatus,
+      quizzConfigSets: question.quizzConfigSets,
+      teacherIds: question.teacherIds,
+      testId: question.testId,
+      testQuestionAnswers: question.testQuestionAnswers,
+      testQuestionGroupId: question.testQuestionGroupId,
+      testQuestionGroupName: question.testQuestionGroupName,
+      testQuestionTypeCode: question.testQuestionTypeCode,
+      testQuestionTypeName: question.testQuestionTypeName,
+      totalFiltered: question.totalFiltered,
+      
     })
   }
 
@@ -93,4 +160,6 @@ export class ChiTietCauHoiComponent implements OnInit {
       }
     })
   }
+
+
 }
