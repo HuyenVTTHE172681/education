42c22e0602e316f1b94becd7e6238feccbeb35e9
Huyen Vu <huyenvtthe172681@fpt.edu.vn>
2024-12-27 16:46:42 +0700

    get data for select in chi tiet cau hoi

--- src/app/pages/admin-page/chi-tiet-cau-hoi/chi-tiet-cau-hoi.component.ts
+++ src/app/pages/admin-page/chi-tiet-cau-hoi/chi-tiet-cau-hoi.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { QuestionsService } from '../../../core/services/question.service';
-import { TestQuestionNewById } from '../../../core/models/question.model';
+import { TestQuestionGroup, TestQuestionNewById, TestQuestionType } from '../../../core/models/question.model';
 import { ActivatedRoute } from '@angular/router';
+import { Teacher } from '../../../core/models/teacher.model';
+import { TeacherService } from '../../../core/services/teacher.service';
 
 @Component({
   selector: 'app-chi-tiet-cau-hoi',
@@ -13,16 +15,36 @@ export class ChiTietCauHoiComponent implements OnInit {
   breadcrumb: MenuItem[] = [];
   home: MenuItem = [];
   id: string | null = null;
-
+  query = {
+    filter: '',
+    page: 1,
+    size: 10000,
+    status: 1
+  }
+  levelQuestion = [
+    { name: 'Tất cả', value: -1 },
+    { name: 'Dễ', value: 1 },
+    { name: 'Trung bình', value: 2 },
+    { name: 'Khó', value: 3 },
+    { name: 'Rất khó', value: 4 },
+  ]
+  teacher: Teacher[] = [];
+  testQuestionType: TestQuestionType[] = [];
+  testQuestionGroup: TestQuestionGroup[] = [];
   questionNew: TestQuestionNewById = new TestQuestionNewById();
 
   constructor(
     private questionsSrv: QuestionsService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private teacherSrv: TeacherService,
+    private questionSrv: QuestionsService
   ) { }
 
   ngOnInit(): void {
     this.initParams();
+    this.getTeacher();
+    this.getTestQuestionType();
+    this.getTestQuestionGroup();
 
     this.route.params.subscribe((params) => {
       this.id = params['id'];
@@ -47,6 +69,28 @@ export class ChiTietCauHoiComponent implements OnInit {
     this.questionsSrv.getTestQuestionNewById(id).subscribe((data) => {
       this.questionNew = data.data;
       console.log("Question Test New By id: ", this.questionNew);
+    })
+  }
+
+  getTeacher() {
+    this.teacherSrv.getTeachers(this.query.page, this.query.size, this.query.filter).subscribe((data) => {
+      this.teacher = data.data.data;
+    })
+  }
+
+  getTestQuestionType() {
+    this.questionSrv.getTestQuestionType(this.query.filter, this.query.page, this.query.size).subscribe(res => {
+      if (res.statusCode === 200) {
+        this.testQuestionType = res.data.data;
+      }
+    })
+  }
+
+  getTestQuestionGroup() {
+    this.questionSrv.getTestQuestionGroup(this.query.filter, this.query.page, this.query.size, this.query.status).subscribe(res => {
+      if (res.statusCode === 200) {
+        this.testQuestionGroup = res.data.data;
+      }
     })
   }
 }
