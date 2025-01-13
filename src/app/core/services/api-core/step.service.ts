f3ab68c18e73cf13e3b3afae0cd6c683d454a4d6
Huyen Vu <huyenvtthe172681@fpt.edu.vn>
2025-01-13 14:31:21 +0700

    update

--- src/app/core/services/api-core/step.service.ts
+++ src/app/core/services/api-core/step.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { API_URL } from '../../../environments/constants';
+import { IResponseList } from '../../models/common.model';
+import { NewsItemStep, Slide } from '../../models/slide.model';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +17,7 @@ export class StepService {
   }
 
   getSteps(): Observable<any[]> {
+    // https://hhq.runasp.net/api/step?filter=&isParent&offSet=0&pageSize=10&screen&status=-1
     return this.http
       .get<any>(
         `${this.apiBaseUrl}/step?filter=&isParent&offSet=0&pageSize=10&screen=trang-chu&status=1`
@@ -31,5 +34,57 @@ export class StepService {
           }));
         })
       );
+  }
+
+  getStep(
+    filter: string,
+    isParent: number,
+    page: number,
+    size: number,
+    screen: string,
+    status: number
+  ): Observable<IResponseList<NewsItemStep>> {
+
+    const query = `/step?filter=${filter}&isParent=${isParent}
+                    &offSet=${(page - 1) * size}&pageSize=${size}&screen=${screen}&status=${status}`;
+
+    const apiURL = `${this.apiBaseUrl}${query}`;
+
+    return this.http
+      .get<IResponseList<NewsItemStep>>(apiURL)
+      .pipe(catchError(this.handleError));
+  }
+
+  getSlide(
+    filter: string,
+    page: number,
+    size: number,
+    screen: string,
+    status: number
+  ): Observable<IResponseList<Slide>> {
+
+    // https://hhq.runasp.net/api/Slide?filter=&offSet=0&pageSize=10&screen=&status=-1
+    const query = `/Slide?filter=${filter}&offSet=${(page - 1) * size}&pageSize=${size}&screen=${screen}&status=${status}`;
+
+    const apiURL = `${this.apiBaseUrl}${query}`;
+
+    return this.http
+      .get<IResponseList<Slide>>(apiURL)
+      .pipe(catchError(this.handleError));
+  }
+
+  // Hàm xử lý lỗi
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${JSON.stringify(error.error)}`
+      );
+    }
+    return throwError(
+      () => new Error('Something went wrong; please try again later.')
+    );
   }
 }
