f24d007ad5f39468bbacff7314f1a50d47b3897e
Huyen Vu <huyenvtthe172681@fpt.edu.vn>
2025-01-13 14:30:34 +0700

    slide

--- src/app/core/models/slide.model.ts
+++ src/app/core/models/slide.model.ts
@@ -1,15 +1,74 @@
 // Model for Slide
 export class Slide {
-  id!: string;
-  name!: string;
-  imageUrl?: string;
-  isVideo!: number;
-  link?: string;
-  screen?: string;
-  status!: number;
-  order!: number;
-  createdBy?: string | null;
-  createdDate?: string | null;
-  modifiedBy?: string | null;
-  modifiedDate?: string | null;
+  id = '';
+  name = '';
+  imageUrl = '';
+  isVideo = 0;
+  link = '';
+  screen = '';
+  status = 0;
+  order =  1;
+  createdBy = '';
+  createdDate = '';
+  modifiedBy = '';
+  modifiedDate = '';
+  totalFiltered = 0;
 }
+
+export class Step {
+  createdBy = '';
+  createdDate = '';
+  delay = 0;
+  description = '';
+  id = '';
+  image = '';
+  isParent = '';
+  modifiedBy = '';
+  modifiedDate = '';
+  name = '';
+  order = 0;
+  screen = '';
+  showNavigation = 0;
+  status = 0;
+  subTitle = '';
+  theme = '';
+  title = '';
+  totalFiltered = 0
+}
+
+export class NewsItemStep {
+  createdBy = '';
+  createdDate = '';
+  delay = 0;
+  description = '';
+  id = '';
+  image = '';
+  isParent = '';
+  modifiedBy = '';
+  modifiedDate = '';
+  name = '';
+  order = 0;
+  screen = '';
+  showNavigation = 0;
+  status = 0;
+  subTitle = '';
+  theme = '';
+  title = '';
+  totalFiltered = 0;
+  steps: Step[] = [];
+}
+
+export class Footer {
+  content = '';
+  createdBy = '';
+  createdDate = '';
+  id = '';
+  modifiedBy = '';
+  modifiedDate = '';
+  order = 1;
+  position = '';
+  status = 0;
+  title = '';
+  totalFiltered = 1;
+}
+
