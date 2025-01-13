2627ffb2d724368e1886be20de35b235a704e1de
Huyen Vu <huyenvtthe172681@fpt.edu.vn>
2025-01-13 14:31:01 +0700

    update

--- src/app/core/services/api-core/footer.service.ts
+++ src/app/core/services/api-core/footer.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError, filter } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { API_URL } from '../../../environments/constants';
+import { IResponseList } from '../../models/common.model';
+import { Footer } from '../../models/slide.model';
 
 @Injectable({
   providedIn: 'root',
@@ -14,18 +16,34 @@ export class FooterService {
     this.apiBaseUrl = API_URL.URL_API_CORE;
   }
 
-  getFooter(): Observable<any[]> {
+  getFooter(
+    filter: string,
+    page: number,
+    size: number
+  ): Observable<IResponseList<Footer>> {
+
+    // https://hhq.runasp.net/api/footer?filter=&offSet=0&pageSize=10
+    const query = `/footer?filter=${filter}&offSet=${(page - 1) * size}&pageSize=${size}`;
+
+    const apiURL = `${this.apiBaseUrl}${query}`;
+
     return this.http
-      .get<any>(`${this.apiBaseUrl}/footer?filter=&offSet=0&pageSize=1000`)
-      .pipe(
-        map((response) => {
-          return response.data.data.map((item: any) => ({
-            content: item.content,
-            title: item.title,
-            position: item.position,
-            id: item.id,
-          }));
-        })
+      .get<IResponseList<Footer>>(apiURL)
+      .pipe(catchError(this.handleError));
+  }
+
+  // Hàm xử lý lỗi
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${JSON.stringify(error.error)}`
       );
+    }
+    return throwError(
+      () => new Error('Something went wrong; please try again later.')
+    );
   }
 }
